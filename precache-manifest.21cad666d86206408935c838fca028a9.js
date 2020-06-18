be9d2b0d2bd1d65c9bbc88d8719609302b1be783
Ibrahym97 <gasanovibragim693@gmail.com>
2020-06-18 07:00:16 +0100

    Updates

---
+++ precache-manifest.21cad666d86206408935c838fca028a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c3e20ca93bf41f45f8fd9b6df9446b",
+    "url": "/fishing2/index.html"
+  },
+  {
+    "revision": "99d76f73f50f26f4add1",
+    "url": "/fishing2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b4a9bb05c2a41b5bbb6",
+    "url": "/fishing2/static/js/2.fa7e1afc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fishing2/static/js/2.fa7e1afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d76f73f50f26f4add1",
+    "url": "/fishing2/static/js/main.6aced29f.chunk.js"
+  },
+  {
+    "revision": "cf070e9b5fd065878450",
+    "url": "/fishing2/static/js/runtime-main.65304c4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fishing2/static/media/logo.5d5d9eef.svg"
+  }
+]);
